005bf3c14c1812b8a08afe5888fb39f7a87bab21
AngelPlayer <ssh5212@naver.com>
2021-12-11 20:26:37 +0900

    Code Upload

---
+++ PC2/client.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERIP   "127.0.0.1"
+#define SERVERPORT 9999
+#define BUFSIZE    512
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    printf("[%s] %s", msg, (char*)lpMsgBuf);
+    LocalFree(lpMsgBuf);
+}
+
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char* buf, int len, int flags)
+{
+    int received;
+    char* ptr = buf;
+    int left = len;
+
+    while (left > 0) {
+        received = recv(s, ptr, left, flags);
+        if (received == SOCKET_ERROR)
+            return SOCKET_ERROR;
+        else if (received == 0)
+            break;
+        left -= received;
+        ptr += received;
+    }
+
+    return (len - left);
+}
+
+int main(int argc, char* argv[])
+{
+
+    int retval;
+
+    // 윈속 초기화
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
+
+    // socket()
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) err_quit("socket()");
+
+    // connect()
+    SOCKADDR_IN serveraddr;
+    ZeroMemory(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+    serveraddr.sin_port = htons(SERVERPORT);
+    retval = connect(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+    if (retval == SOCKET_ERROR) err_quit("connect()");
+
+    // 데이터 통신에 사용할 변수
+    char buf[BUFSIZE + 1];
+
+    // 서버와 데이터 통신
+    while (1) {
+
+
+        // 접속한 클라이언트 정보 출력
+        printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+            inet_ntoa(serveraddr.sin_addr), ntohs(serveraddr.sin_port));
+
+        // 클라이언트와 데이터 통신
+        while (1) {
+            // 데이터 받기
+            retval = recv(sock, buf, BUFSIZE, 0);
+            if (retval == SOCKET_ERROR) {
+                err_display("recv()");
+                break;
+            }
+            else if (retval == 0)
+                break;
+
+
+            // 받은 데이터 출력
+            buf[retval] = '\0';
+            printf("[TCP/%s:%d] %s\n", inet_ntoa(serveraddr.sin_addr),
+                ntohs(serveraddr.sin_port), buf);
+
+
+            FILE* fp = fopen("result.txt", "a");    // result.txt 파일을 추가 모드(a)로 열기. (!미리 생성해두기!)
+                                                    // 파일 포인터를 반환
+
+            fputs(buf, fp);                         // 파일에 문자열 저장
+
+            fclose(fp);                             // 파일 포인터 닫기
+        }
+
+        // closesocket()
+        closesocket(sock);
+        printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+            inet_ntoa(serveraddr.sin_addr), ntohs(serveraddr.sin_port));
+    }
+
+    // closesocket()
+    closesocket(sock);
+
+    // 윈속 종료
+
+    WSACleanup();
+    return 0;
+}
